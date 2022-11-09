f1d7300fbe07a28f74a3633e1d77108c1fd152c6
Marc LANTOL <marc.lantol@gmail.com>
2022-11-09 10:47:55 +0100

    Import skills

---
+++ src/Datas/Hardskills.js
@@ -0,0 +1,47 @@
+import js from "../assets/hardskills/js.png";
+import html from "../assets/hardskills/html.png";
+import notion from "../assets/hardskills/notion.png";
+import node from "../assets/hardskills/node.png";
+import express from "../assets/hardskills/express.png";
+import git from "../assets/hardskills/git.png";
+import css from "../assets/hardskills/css.png";
+
+const HardSkills = [
+  {
+    id: 1,
+    image: js,
+    texte: "Javascript",
+  },
+  {
+    id: 2,
+    image: html,
+    texte: "Html",
+  },
+  {
+    id: 3,
+    image: notion,
+    texte: "Notion",
+  },
+  {
+    id: 4,
+    image: node,
+    texte: "Node",
+  },
+  {
+    id: 5,
+    image: express,
+    texte: "Express",
+  },
+  {
+    id: 6,
+    image: git,
+    texte: "Git",
+  },
+  {
+    id: 7,
+    image: css,
+    texte: "Css",
+  },
+];
+
+export default HardSkills;
