b1e20109e0e154a736fff59f46b4ef10d3c797e2
Marc LANTOL <marc.lantol@gmail.com>
2022-11-16 13:49:52 +0100

    Fix buttons placement

--- src/components/ButtonMode.jsx
+++ src/components/ButtonMode.jsx
@@ -7,7 +7,7 @@ const ButtonMode = () => {
     <div>
       <div>
         <input
-          className="fixed -ml-16 appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-gray-300 rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
+          className="relative appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-gray-300 rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
           type="checkbox"
           role="switch"
           id="flexSwitchCheckDefault"
@@ -16,7 +16,7 @@ const ButtonMode = () => {
           }}
         ></input>
         <label
-          className="fixed -ml-5 form-check-label inline-block mx-11 -my-1"
+          className="fixed form-check-label inline-block mx-11 -my-1"
           htmlFor="flexSwitchCheckDefault"
         >
           {mode === "light" ? "Light" : "Dark"}
