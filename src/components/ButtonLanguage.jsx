2f2c5366ee2ef82fc47211f8ce1fbf61a4c80fc5
Marc LANTOL <marc.lantol@gmail.com>
2022-11-16 15:30:23 +0100

    Fix button colors and style

--- src/components/ButtonLanguage.jsx
+++ src/components/ButtonLanguage.jsx
@@ -4,24 +4,30 @@ import traduction from "../Datas/Traduction";
 
 const ButtonLanguage = () => {
   const { switchLang, language } = useContext(LangContext);
+
+  const lang = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-10 h-10 text-stone-500" >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+    </svg >
+  )
+
   return (
     <div>
       <div>
-        <input
-          className="relative appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-gray-300 rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
-          type="checkbox"
-          role="switch"
-          id="flexSwitchCheckDefault"
-          onClick={(e) => {
-            language == "fr" ? switchLang("en") : switchLang("fr");
-          }}
-        ></input>
-        <label
-          className="fixed form-check-label inline-block mx-11 -my-1"
-          htmlFor="flexSwitchCheckDefault"
-        >
-          {traduction[language].buttonLabel}
-        </label>
+        <button type="button" onClick={() => {
+          language == "fr" ? switchLang("en") : switchLang("fr");
+        }} className="right-20 top-4 text-lg rounded-md">
+          {lang}
+        </button>
       </div>
     </div>
   );
