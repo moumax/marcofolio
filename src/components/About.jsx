ff2fc59b465ff99d1923da122ad83267528499ec
Marc LANTOL <marc.lantol@gmail.com>
2022-11-22 14:16:38 +0100

    Add text and clean code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,14 @@
 import { LangContext } from "../context/LangContext";
 import { useContext } from "react";
 
+import { motion } from "framer-motion";
+
 import traduction from "../Datas/dataTraduction";
 import DataSoftSkills from "../Datas/dataSoftskills";
 import DatasHardSkills from "../Datas/dataHardskills";
 import DataHobbies from "../Datas/dataHobbies";
 
-import Softskill from "./utils/Softskill"
+import Softskill from "./utils/Softskill";
 import Hardskill from "./utils/Hardskill";
 import Hobby from "./utils/Hobby";
 
@@ -14,14 +16,20 @@ const About = () => {
   const { language } = useContext(LangContext);
 
   const classNameTitle = {
-    className: "text-start ml-2 text-2xl my-6 underline 2xl:text-4xl xs:text-xl",
+    className:
+      "text-start ml-2 text-2xl my-6 underline 2xl:text-4xl xs:text-xl",
   };
   const classNameSkills = {
     className: "flex flex-wrap gap-2 justify-center 2xl:gap-20 2xl:w-fit",
   };
 
   return (
-    <section className="flex flex-col items-center relative h-fit mb-16 ">
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="flex flex-col items-center relative h-fit mb-16 "
+    >
       <h2 className={classNameTitle.className}>
         {traduction[language].softSkillTitle}
       </h2>
@@ -46,7 +54,7 @@ const About = () => {
           <Hobby like={like} key={like.id} />
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import { LangContext } from "../context/LangContext";
-import { useContext } from "react";
+import { useContext} from "react";
 import { motion } from "framer-motion";
 
 import dataTraduction from "../Datas/dataTraduction";
+
 
 const Home = () => {
   const { language } = useContext(LangContext);
@@ -11,20 +12,28 @@ const Home = () => {
     <section className="relative h-screen ml-5 mr-5 flex flex-col justify-center pb-48">
       <motion.h1
         initial={{ opacity: 0, x: -800 }}
-        animate={{ opacity: 1, x: 0, }}
-        transition={{ delay: 0.5, duration:0.4 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.5, duration: 0.4 }}
         className="text-5xl mb-10 mt-24"
       >
         {dataTraduction[language].bonjour}
       </motion.h1>
       <motion.h2
         initial={{ opacity: 0, x: "100vw" }}
-        animate={{ opacity: 1, x: 0, }}
-        transition={{ delay: 0.3, duration:1}}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.3, duration: 1 }}
         className="text-black-600 text-2xl"
       >
         {dataTraduction[language].me}
       </motion.h2>
+      <motion.h3
+        className="mt-10"
+        initial={{ opacity: 0, blur: 1000 }}
+        animate={{ opacity: 1, blur: 0 }}
+        transition={{ delay: 2, duration: 0.6 }}
+      >
+        {dataTraduction[language].search}
+      </motion.h3>
     </section>
   );
 };
