d885a6f1b6e7e3671bc1fa75e01e0dc6a82c2c8a
Marc LANTOL <marc.lantol@gmail.com>
2022-11-23 12:28:51 +0100

    Responsive for home Ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
   const { language } = useContext(LangContext);
 
   return (
-    <section className="relative h-screen ml-5 mr-5 flex flex-col justify-center pb-48 z-0">
+    <section className="relative h-screen ml-5 mr-5 flex flex-col justify-center pb-48 z-0 md:gap-14">
       <motion.h1
         initial={{ opacity: 0, x: -800 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.5, duration: 0.4 }}
-        className="text-5xl mb-10 mt-24"
+        className="text-5xl mb-10 mt-24 sm:text-6xl xl:text-7xl"
       >
         {dataTraduction[language].bonjour}
       </motion.h1>
@@ -22,15 +22,15 @@ const Home = () => {
         initial={{ opacity: 0, x: "100vw" }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.3, duration: 1 }}
-        className="text-black-600 text-2xl"
+        className="text-black-600 text-2xl sm:text-3xl xl:text-4xl"
       >
         {dataTraduction[language].me}
       </motion.h2>
       <motion.h3
-        className="mt-10"
         initial={{ opacity: 0, blur: 1000 }}
         animate={{ opacity: 1, blur: 0 }}
         transition={{ delay: 2, duration: 0.6 }}
+        className="mt-10 sm:text-2xl xl:text-3xl"
       >
         {dataTraduction[language].search}
       </motion.h3>
