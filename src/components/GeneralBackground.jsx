4c9f1daf0b7f1e778a3837b5cba9155ab2e545b1
Marc LANTOL <marc.lantol@gmail.com>
2022-11-26 11:57:19 +0100

    Refacto General background

--- src/components/GeneralBackground.jsx
+++ src/components/GeneralBackground.jsx
@@ -3,17 +3,16 @@ import { ModeContext } from "../context/ModeContext";
 
 const GeneralBackground = () => {
   const { mode } = useContext(ModeContext);
+
+  const classBackgroundDark =
+    "absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black w-screen h-full z-0";
+  const classBackgroundLight = "absolute bg-white w-screen h-full z-0";
+
   return (
     <div
-      className={
-        mode === "light"
-          ? "absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black w-screen h-full z-0"
-          : "absolute bg-white w-screen h-full z-0"
-      }
+      className={mode === "light" ? classBackgroundDark : classBackgroundLight}
     ></div>
   );
 };
 
 export default GeneralBackground;
-
-
