27d45fcd56910840cc010a57a21b7764a981dce9
Marc LANTOL <marc.lantol@gmail.com>
2022-12-02 15:20:19 +0100

    Fix project responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,15 +11,14 @@ import dataPortfolio from "../Datas/dataPortfolio";
 import Contact from "./Contact.jsx";
 
 const Projects = () => {
-  const { language } = useContext(LangContext);
   const { mode } = useContext(ModeContext);
   const [isShown, setIsShown] = useState(false);
 
   const classBorderDark =
-    "w-fit flex flex-col items-center border-2 border-slate-800 rounded-md overflow-hidden m-10";
+    "flex flex-col border-2 border-slate-800 rounded-md h-full";
   const classBorderLight =
     "w-fit flex flex-col items-center border-2 border-white rounded-md overflow-hidden";
-  const classImg = "h-fit";
+  const classImg = "px-2 ";
   const classLabelTechno =
     "border-2 border-stone-900 rounded-md hover:text-blue-300";
 
@@ -34,86 +33,89 @@ const Projects = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.2, duration: 2 }}
-          className="relative w-screen flex flex-col h-screen justify-center px-5 sm:px-10 xl:px-36"
+          className="relative w-screen"
         >
-          <h1 className="absolute top-0 mb-20 left-3 text-3xl mt-11 underline  bg-gradient-to-r from-orange-400 to-blue-800 bg-clip-text text-transparent">
-            {traduction[language].projectTitle}
-          </h1>
-          <Carousel
-            infiniteLoop
-            autoPlay
-            interval={4000}
-            showStatus={false}
-            showIndicators={false}
-            showThumbs={false}
-            className="relative pb-10 w-full lg:w-2/3 lg:mx-auto"
-          >
-            {dataPortfolio.map((data) => (
-              <div
-                key={data.titre}
-                className={mode === "dark" ? classBorderDark : classBorderLight}
-              >
-                <h2 className="text-start text-2xl mt-3 mb-5 ml-4 w-fit underline">{data.titre}</h2>
-                <a href={data.lien} target="_blank" rel="noopener noreferrer">
-                  {mode == "dark" ? (
-                    <img
-                      className={classImg}
-                      src={data.imageLight || data.image}
-                      alt={data.imageLight}
-                    />
-                  ) : (
-                    <img
-                      className={classImg}
-                      src={data.imageDark || data.image}
-                      alt={data.imageDark}
-                    />
-                  )}
-                </a>
-                <ul className="flex flex-wrap justify-center gap-2 mb-7 text-md mt-7">
-                  {data.techno1 ? (
-                    <li className={classLabelTechno}>
-                      <span className="m-2">{data.techno1}</span>
-                    </li>
-                  ) : null}
+          <div className="flex flex-col w-fit mx-auto">
+            <Carousel
+              infiniteLoop
+              autoPlay
+              interval={4000}
+              showStatus={false}
+              showIndicators={false}
+              showThumbs={false}
+              className="relative w-[90vw] "
+            >
+              {dataPortfolio.map((data) => (
+                <div
+                  key={data.titre}
+                  className={
+                    mode === "dark" ? classBorderDark : classBorderLight
+                  }
+                >
+                  <h2 className="text-start text-2xl mt-3 mb-5 ml-4 w-fit underline">
+                    {data.titre}
+                  </h2>
+                  <a href={data.lien} target="_blank" rel="noopener noreferrer">
+                    {mode == "dark" ? (
+                      <img
+                        className={classImg}
+                        src={data.imageLight || data.image}
+                        alt={data.imageLight}
+                      />
+                    ) : (
+                      <img
+                        className={classImg}
+                        src={data.imageDark || data.image}
+                        alt={data.imageDark}
+                      />
+                    )}
+                  </a>
+                  <ul className="flex flex-wrap justify-center gap-2 mb-7 text-md mt-7">
+                    {data.techno1 ? (
+                      <li className={classLabelTechno}>
+                        <span className="m-2">{data.techno1}</span>
+                      </li>
+                    ) : null}
 
-                  {data.techno2 ? (
-                    <li className={classLabelTechno}>
-                      <span className="m-2">{data.techno2}</span>
-                    </li>
-                  ) : null}
+                    {data.techno2 ? (
+                      <li className={classLabelTechno}>
+                        <span className="m-2">{data.techno2}</span>
+                      </li>
+                    ) : null}
 
-                  {data.techno3 ? (
-                    <li className={classLabelTechno}>
-                      <span className="m-2">{data.techno2}</span>
-                      <span className="m-2">{data.techno3}</span>
-                    </li>
-                  ) : null}
+                    {data.techno3 ? (
+                      <li className={classLabelTechno}>
+                        <span className="m-2">{data.techno2}</span>
+                        <span className="m-2">{data.techno3}</span>
+                      </li>
+                    ) : null}
 
-                  {data.techno4 ? (
-                    <li className={classLabelTechno}>
-                      <span className="m-2">{data.techno4}</span>
-                    </li>
-                  ) : null}
+                    {data.techno4 ? (
+                      <li className={classLabelTechno}>
+                        <span className="m-2">{data.techno4}</span>
+                      </li>
+                    ) : null}
 
-                  {data.techno5 ? (
-                    <li className={classLabelTechno}>
-                      <span className="m-2">{data.techno5}</span>
-                    </li>
-                  ) : null}
-                </ul>
-              </div>
-            ))}
-          </Carousel>
-          <motion.button
-            initial={{ opacity: 0, x: "100vw" }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 1, duration: 0.6 }}
-            className="w-fit border-2 border-slate-500 rounded px-4 py-1 self-center sm:text-xl"
-            type={"button"}
-            onClick={affiche}
-          >
-            Me contacter
-          </motion.button>
+                    {data.techno5 ? (
+                      <li className={classLabelTechno}>
+                        <span className="m-2">{data.techno5}</span>
+                      </li>
+                    ) : null}
+                  </ul>
+                </div>
+              ))}
+            </Carousel>
+            <motion.button
+              initial={{ opacity: 0, x: "100vw" }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 1, duration: 0.6 }}
+              className="border-2 border-slate-500 rounded px-4 py-1 mx-auto mt-5"
+              type={"button"}
+              onClick={affiche}
+            >
+              Me contacter
+            </motion.button>
+          </div>
         </motion.section>
       )}
       {isShown && <Contact />}
